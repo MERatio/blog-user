37eabd9e611064e1136a90433ed6a8e984280dea
Mark Edward Ratio <markedwardratio@gmail.com>
2021-03-05 12:10:08 +0800

    Redirect root path to /posts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect,
+} from 'react-router-dom';
 import Navbar from './components/Navbar';
+
 import './App.css';
 
 function App() {
 	return (
 		<Router basename={process.env.PUBLIC_URL}>
 			<Navbar />
+			<main>
+				<Switch>
+					<Route exact path="/">
+						<Redirect to="/posts" />
+					</Route>
+				</Switch>
+			</main>
 		</Router>
 	);
 }
