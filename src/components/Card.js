146d35f2a76a4315e6d656b74eb890940f13ffe1
Mark Edward Ratio <markedwardratio@gmail.com>
2021-03-06 12:15:48 +0800

    Add Card card-header element

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,14 +5,16 @@ import format from 'date-fns/format';
 function Card({ item }) {
 	return (
 		<div className="card">
-			<div className="card-body">
+			<div className="card-header">
 				<h5 className="card-title">{item.title}</h5>
 				<p className="card-subtitle mb-2">
 					{item.author.firstName + ' ' + item.author.lastName}
 				</p>
-				<p className="card-subtitle mb-2 text-muted">
+				<p className="card-subtitle">
 					{format(new Date(item.createdAt), 'PPpp')}
 				</p>
+			</div>
+			<div className="card-body">
 				<p className="card-text">{item.body}</p>
 				<Link to={`/posts/${item._id}`} className="card-link">
 					{item.commentsCount} comments
