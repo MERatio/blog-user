793a833b171aa92ed98131a2a92b73d9e04982b7
Mark Edward Ratio <markedwardratio@gmail.com>
2021-03-06 12:08:44 +0800

    Add date in PostComments

--- src/components/PostComments.js
+++ src/components/PostComments.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import format from 'date-fns/format';
 
 function PostComments({ postId }) {
 	const [postComments, setPostComments] = useState([]);
@@ -20,8 +21,11 @@ function PostComments({ postId }) {
 		<ul className="list-group">
 			{postComments.map((postComment) => (
 				<li key={postComment._id} className="list-group-item">
-					<p className="h6 mb-2">
+					<p className="h6 mb-1">
 						{postComment.author.firstName + ' ' + postComment.author.lastName}
+					</p>
+					<p className="text-muted mb-2">
+						{format(new Date(postComment.createdAt), 'PPpp')}
 					</p>
 					<p className="mb-2">{postComment.body}</p>
 				</li>
