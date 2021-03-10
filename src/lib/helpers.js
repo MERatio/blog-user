e28cdb91d6d242f1b1c435ca32c90adf96485f25
Mark Edward Ratio <markedwardratio@gmail.com>
2021-03-10 15:17:26 +0800

    Add getData helper

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,3 +1,13 @@
+async function getData(url = '') {
+	const response = await fetch(url, {
+		method: 'GET',
+		headers: {
+			Authorization: `Bearer ${localStorage.getItem('jwt')}`,
+		},
+	});
+	return response.json();
+}
+
 async function postData(url = '', data = {}) {
 	const response = await fetch(url, {
 		method: 'POST',
@@ -13,4 +23,4 @@ function handleExpressErr(err) {
 	window.flashes([{ msg: err.message }]);
 }
 
-export { postData, handleExpressErr };
+export { getData, postData, handleExpressErr };
