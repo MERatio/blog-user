a7920e2f37f9c703f416497be36f682a97928a60
Mark Edward Ratio <markedwardratio@gmail.com>
2021-03-12 11:57:10 +0800

    Refactor Posts CDM

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -7,33 +7,53 @@ function Posts() {
 	const [postsWithComments, setPostsWithComments] = useState([]);
 
 	useEffect(() => {
-		async function fetchAndSetPostsWithComments() {
+		async function fetchPosts() {
 			try {
 				const data = await getData(`${process.env.REACT_APP_API_URL}/posts`);
-				const posts = data.posts;
 				if (data.err) {
 					handleExpressErr(data.err);
-					return;
+				} else {
+					return data.posts;
 				}
-
-				const newPostsWithComments = await Promise.all(
-					posts.map(async (post) => {
-						const data = await getData(
-							`${process.env.REACT_APP_API_URL}/posts/${post._id}/comments`
-						);
-						if (data.err) {
-							handleExpressErr(data.err);
-							return;
-						}
-						return { ...post, comments: data.comments };
-					})
-				);
-				setPostsWithComments(newPostsWithComments);
 			} catch (err) {
 				window.flashes([
 					{ msg: 'Something went wrong, please try again later.' },
 				]);
 			}
+		}
+
+		async function fetchAndAttachPostsToComments(posts) {
+			try {
+				const postsWithComments = await Promise.all(
+					posts.map(async (post) => {
+						try {
+							const data = await getData(
+								`${process.env.REACT_APP_API_URL}/posts/${post._id}/comments`
+							);
+							if (data.err) {
+								handleExpressErr(data.err);
+							} else {
+								return { ...post, comments: data.comments };
+							}
+						} catch (err) {
+							window.flashes([
+								{ msg: 'Something went wrong, please try again later.' },
+							]);
+						}
+					})
+				);
+				return postsWithComments;
+			} catch (err) {
+				window.flashes([
+					{ msg: 'Something went wrong, please try again later.' },
+				]);
+			}
+		}
+
+		async function fetchAndSetPostsWithComments() {
+			const posts = await fetchPosts();
+			const newPostsWithComments = await fetchAndAttachPostsToComments(posts);
+			setPostsWithComments(newPostsWithComments);
 		}
 
 		fetchAndSetPostsWithComments();
