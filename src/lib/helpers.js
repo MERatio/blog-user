d8b1142c50baa6660de415476f275f585bb65bfe
Mark Edward Ratio <markedwardratio@gmail.com>
2021-03-24 11:10:53 +0800

    Add try catch to getData and postData helpers

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,23 +1,31 @@
 async function getData(url = '') {
-	const response = await fetch(url, {
-		method: 'GET',
-		headers: {
-			Authorization: `Bearer ${localStorage.getItem('jwt')}`,
-		},
-	});
-	return response.json();
+	try {
+		const response = await fetch(url, {
+			method: 'GET',
+			headers: {
+				Authorization: `Bearer ${localStorage.getItem('jwt')}`,
+			},
+		});
+		return response.json();
+	} catch (err) {
+		window.flashes([{ msg: 'Something went wrong, please try again later.' }]);
+	}
 }
 
 async function postData(url = '', data = {}) {
-	const response = await fetch(url, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: `Bearer ${localStorage.getItem('jwt')}`,
-		},
-		body: JSON.stringify(data),
-	});
-	return response.json();
+	try {
+		const response = await fetch(url, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${localStorage.getItem('jwt')}`,
+			},
+			body: JSON.stringify(data),
+		});
+		return response.json();
+	} catch (err) {
+		window.flashes([{ msg: 'Something went wrong, please try again later.' }]);
+	}
 }
 
 function handleExpressErr(err) {
